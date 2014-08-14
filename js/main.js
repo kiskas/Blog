f6abc2f5da8e3aaa4278926d093f322700e8ca00
Angel <angelinkas@inbox.ru>
2014-08-14 12:46:20 +0400

    add files

--- js/main.js
+++ js/main.js
@@ -5,5 +5,18 @@ $(function () {
         nav: true,
         fade: 500
     });
+	
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$('.up').fadeIn();
+		}else {
+			$('.up').fadeOut();
+		}
+	});
+	
+	$('.up').click(function () {
+		$('body,html').animate({scrollTop: 0}, 400); 
+		return false;
+	});
 
 });
